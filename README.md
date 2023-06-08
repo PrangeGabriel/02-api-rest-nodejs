83c8e35138216b281e911eb7d9c7536e734e05e9
Gabriel Prange <67351346+PrangeGabriel@users.noreply.github.com>
2023-06-07 23:46:08 -0300

    tipagem do knex

---
+++ README.md
@@ -0,0 +1,13 @@
+# Requisitos Funcionais
+
+[] o usuario deve poder criar uma nova transação;
+[] o usuario deve poder obter um resumo da sua conta;
+[] deve poder listar todas as transações que ja ocorreram;
+[] deve poder uma transação unica 
+
+# Requisitos Não Funcionais
+
+[] a transação pode ser do tipo credito que soma o valor total ou debito q subtrai;
+[] deve ser possivel identificar o usuario entre as requisições;
+[] o usuario so pode visualizar transações que ele criou
+
---
+++ src/routes/transactions.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import crypto from 'node:crypto'
+
+export async function transactionRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    // transações tem titulo, valor e tipo
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send('transação feita!')
+  })
+}
